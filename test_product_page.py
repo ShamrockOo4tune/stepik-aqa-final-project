2b457da175607f7c57cf284271344a0fc3134ba9
Shamil Gumerov <shamusg12345@gmail.com>
2023-01-22 18:17:40 +0300

    move go_to_login_page related methods to base_page

--- test_product_page.py
+++ test_product_page.py
@@ -22,3 +22,15 @@ def test_guest_can_add_product_to_basket(browser, link):
     product_page.solve_quiz_and_get_code()
     product_page.product_added_with_correct_product_name(product_name=product_name)
     product_page.product_price_match_basket_total(product_price=product_price)
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
